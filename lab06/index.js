0fd911436dbc1ff0194e5e6c0cd0be3cbd4c149d
Julia Plichta <juliaplichta0@gmail.com>
2021-03-29 13:30:23 +0200

    zad 02

--- lab06/index.js
+++ lab06/index.js
@@ -1,22 +1,47 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/movies", async (req, res) => {
+  const selectMovies = "select * from movie";
+  const response = await client.query(selectMovies);
+  return res.send(response.rows);
+});
+
+app.post("/movies", async (req, res) => {
+  const insertMovie = "INSERT INTO public.movie(name, genre, releaseyear) "
+  + `VALUES ('${req.body.name}', '${req.body.genre}', ${req.body.releaseyear}) RETURNING id;`;
+  
+  try {
+    const result = await client.query(insertMovie);
+    return res.status(200).send(result.rows[0]);
+  } catch {
+    return res.status(404).send("error");
+  }
+});
+
+require("dotenv").config();
+const dbConnData = {
+    host: process.env.PGHOST || "127.0.0.1",
+    port: process.env.PGPORT || 5432,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASSWORD
+};
+
+
 const { Client } = require("pg");
+const client = new Client(dbConnData);
 
-const client = new Client({
-    user: "postgres",
-    host: "localhost",
-    password: "mysecretpassword"
-});
-
-client.connect();
-
-const createTable = 
-"CREATE TABLE Movie ( " +
-"    Id SERIAL, " +
-"    Name VARCHAR NOT NULL, " +
-"    Genre VARCHAR NOT NULL, " +
-"    ReleaseYear INT NOT NULL " +
-")"
-
-client.query(createTable, (err, res) => {
-    console.log(err ? err.stack : res.rows[0]);
-    client.end();
-});
+console.log("Connection parameters: ");
+console.log(dbConnData);
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL");
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`API server listening at http://localhost:${port}`);
+    });
+  })
+  .catch(err => console.error("Connection error", err.stack));
